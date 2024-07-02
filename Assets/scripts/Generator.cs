51635698020886846d6afde7baa6c87e48097499
Tobkubos <tobkub7@gmail.com>
2024-07-03 00:05:17 +0200

    generator 1

---
+++ Assets/scripts/Generator.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject SurfaceTile;
+	public GameObject StartTile;
+	public GameObject EndTile;
+	public GameObject CheckPoint;
+	public GameObject Path;
+	private int StartX, StartY, Rotation;
+	int SizeOfPlate = 20;
+	float tick = 0.05f;
+	private List<string> Directions = new List<string>{ "North", "East", "South", "West" };
+	private List<GameObject> CheckPoints  = new List<GameObject> { };
+	void Start()
+    {
+       StartCoroutine(GenerateTiles());
+    }
+
+    private IEnumerator GenerateTiles()
+	{
+		for (int j = 0; j < SizeOfPlate; j++)
+		{
+				yield return new WaitForSeconds(tick);
+				StartCoroutine(GenerateRow(j));
+		}
+
+		TempGenerate();
+	}
+
+	private IEnumerator GenerateRow(int j)
+	{
+		for (int i = 0; i < SizeOfPlate; i++)
+		{
+			yield return new WaitForSeconds(tick*2);
+			Instantiate(SurfaceTile, new Vector3(i, 0, j), Quaternion.identity);
+		}
+	}
+
+	void GenerateStartEndPoint(GameObject Object)
+	{
+		int RandomSide = Random.Range(0, Directions.Count);
+		string Side = Directions[RandomSide];
+		if (Side == "North")
+		{
+			StartX = Random.Range(2, SizeOfPlate-1);
+			StartY = SizeOfPlate-1;
+			Rotation = 0;
+		}
+		if (Side == "East")
+		{
+			StartX = SizeOfPlate-1;
+			StartY = Random.Range(2, SizeOfPlate-1);
+			Rotation = 90;
+		}
+		if (Side == "South")
+		{
+			StartX = Random.Range(2, SizeOfPlate - 1);
+			StartY = 0;
+			Rotation = 180;
+		}
+		if (Side == "West")
+		{
+			StartX = 0;
+			StartY = Random.Range(2, SizeOfPlate - 1);
+			Rotation = 270;
+		}
+
+		Directions.Remove(Side.ToString());
+		Vector3 Cords = new Vector3(StartX, 0.2f, StartY);
+		GameObject temp = Instantiate(Object, Cords, Quaternion.Euler(0, Rotation, 0));
+		CheckPoints.Add(temp);
+	}
+
+	public void TempGenerate()
+	{
+		Destroy(GameObject.FindGameObjectWithTag("start"));
+		Destroy(GameObject.FindGameObjectWithTag("end"));
+		GenerateStartEndPoint(StartTile);
+		
+		Directions.Clear();
+		Directions = new List<string> { "North", "East", "South", "West" };
+
+		
+		for(int i = 0; i<SizeOfPlate; i++)
+		{
+			int CanPlace = Random.Range(0, 10);
+			if (CanPlace < 3)
+			{
+				int rowCord = Random.Range(1, SizeOfPlate-1);
+				GameObject temp = Instantiate(CheckPoint, new Vector3(i, 0.3f, rowCord), Quaternion.identity);
+				CheckPoints.Add(temp);
+			}
+		}
+		Debug.Log(CheckPoints.Count);
+
+		for (int i = 1; i<CheckPoints.Count; i++)
+		{
+			Debug.Log("KUPA");
+			int xPrev = (int)CheckPoints[i - 1].transform.position.x;
+			int xAcc = (int)CheckPoints[i].transform.position.x;
+
+			int yPrev = (int)CheckPoints[i - 1].transform.position.y;
+			int yAcc = (int)CheckPoints[i].transform.position.y;
+
+			//tutaj chokepoint
+			if ( xPrev != xAcc)
+			{
+				if(xPrev < xAcc)
+				{
+					for(int j = xPrev; j<= xAcc; j++)
+					{
+						Instantiate(Path, new Vector3(j, 0.3f, yPrev), Quaternion.identity);
+					}
+				}
+
+				if (xPrev > xAcc)
+				{
+					for (int j = xAcc; j >= xPrev; j--)
+					{
+						Instantiate(Path, new Vector3(j, 0.3f, yPrev), Quaternion.identity);
+					}
+				}
+
+			}
+
+			if (CheckPoints[i - 1].transform.position.y != CheckPoints[i].transform.position.y)
+			{
+
+			}
+		}
+
+
+		GenerateStartEndPoint(EndTile);
+	}
+}
